da5e42f2d3fca9c329b5542d999afc7c3cb652d1
mihnovitz <mihnovicz@gmail.com>
2024-11-29 18:05:55 +0100

    Initial commit

---
+++ src/Color.java
@@ -0,0 +1,23 @@
+public record Color(int red, int green, int blue, int alpha) {
+
+    public Color(int red, int green, int blue, int alpha) {
+        this.red = red;
+        this.green = green;
+        this.blue = blue;
+        this.alpha = alpha;
+    }
+
+    public Color(int red, int green, int blue) {
+        this(red,green,blue,0);
+    }
+
+    @Override
+    public String toString() {
+        return "Color{" +
+                "red=" + red +
+                ", green=" + green +
+                ", blue=" + blue +
+                ", alpha=" + alpha +
+                '}';
+    }
+}
---
+++ src/Main.java
@@ -0,0 +1,16 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        Color Zolty = new Color(1,1,1);
+        Color Braz = new Color(867867,2345,576);
+        Color Fiolet = new Color(12525,23424,3132);
+
+        System.out.println(Zolty);
+        System.out.println(Braz);
+        System.out.println(Fiolet);
+
+        System.out.println(Zolty.red());
+    }
+
+}
